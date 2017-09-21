2ba5b137035d79b64c5c80c43b4ee681aa201b69
nayfin <natjkern@gmail.com>
2017-09-20 22:44:07 -0500

    Passing context to a dynamic template

--- 01-content-projection/app/app.component.ts
+++ 01-content-projection/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ComponentRef, ViewContainerRef, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
+import { Component, ViewChild, ComponentRef, ViewContainerRef, ComponentFactoryResolver, AfterContentInit, TemplateRef } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form/auth-form.component';
 import { User } from './auth-form/auth-form.interface';
@@ -8,7 +8,12 @@ import { User } from './auth-form/auth-form.interface';
   template: `
     <button (click)="destroyComponent()">DELETE FORM</button>
     <div>
-      <div #entry> </div>
+      <ng-container>
+      
+      </ng-container>
+      <template #tmpl let-name let-location="location">
+        {{name}} : {{ location }}
+      </template>
     </div>
   `
 })
@@ -19,17 +24,16 @@ export class AppComponent implements AfterContentInit {
   component: ComponentRef<AuthFormComponent>
 
   @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
-
+  @ViewChild('tmpl') tmpl: TemplateRef<any>;
   constructor( 
     private resolver: ComponentFactoryResolver
   ) { }
 
   ngAfterContentInit() {
-    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.component = this.entry.createComponent(authFormFactory);
-    console.log("component.instance",this.component.instance);
-    this.component.instance.title = 'Create Account';
-    this.component.instance.submitted.subscribe( this.loginUser );
+    this.entry.createEmbeddedView(this.tmpl, {
+      $implicit: 'Bill Murry',
+      location: 'England, UK'
+    })
   }
   rememberUser(remember: boolean) {
     this.rememberMe = remember;
